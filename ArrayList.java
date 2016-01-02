694d644c572284d75d7177732b13ae243031b760
David Jones <davidjones889@googlemail.com>
2016-01-02 13:41:08 +0000

    Added the methods to extend the ArrayList when needed and tested all the methods again

--- ArrayList.java
+++ ArrayList.java
@@ -1,15 +1,19 @@
+import java.util.Arrays;
+
 public class ArrayList implements List {
 
 	private Object[] items;
-	private int size;
+	private int sizeOfArray;
+	private int maximumSizeOfArray;
 
 	ArrayList(int size) {
-		this.items = new Object[size];
-		this.size = size;
+		this.items       		= new Object[size];
+		this.sizeOfArray 		= size;
+		this.maximumSizeOfArray = size;
 	}
 
 	public boolean isEmpty() {
-		for (int i = 0; i < this.size; i++) {
+		for (int i = 0; i < this.maximumSizeOfArray; i++) {
 			if (this.get(i).getReturnValue() != null) {
 				return false;
 			}
@@ -20,7 +24,7 @@ public class ArrayList implements List {
 
 	public int size() {
 		int count = 0;
-		for (int i = 0; i < this.size; i++) {
+		for (int i = 0; i < this.maximumSizeOfArray; i++) {
 			if (this.items[i] != null) {
 				count++;
 			}
@@ -30,7 +34,7 @@ public class ArrayList implements List {
 	}
 
 	public ReturnObject get(int index) {
-		if (index < 0 || index >= this.size()) {
+		if (index < 0 || index >= this.maximumSizeOfArray) {
 			return new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
 		} else {
 			return new ReturnObjectImpl(this.items[index]);
@@ -38,14 +42,18 @@ public class ArrayList implements List {
 	}
 
 	public ReturnObject remove(int index) {
-		if (index < 0 || index >= this.size) {
+		if (index < 0 || index >= this.maximumSizeOfArray) {
 			return new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
 		} else {
 			Object item = this.get(index);
 			
-			for (int i = (index + 1); i < this.size; i++) {
-				int newIndex = i - 1;
-				this.items[newIndex] = this.items[i];
+			for (int i = index; i < this.maximumSizeOfArray; i++) {
+				int targetIndex = i + 1;
+				if (targetIndex >= this.maximumSizeOfArray) {
+					this.items[i] = null;
+				} else {
+					this.items[i] = this.items[targetIndex];
+				}
 			}
 
 			return new ReturnObjectImpl(item);
@@ -56,19 +64,22 @@ public class ArrayList implements List {
 
 		if (object == null) {
 			return new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT);
-		} else if (index >= this.size || index < 0 || this.size() >= this.size) {
+		} else if (index >= this.maximumSizeOfArray || index < 0) {
 			return new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
 		} else {
+			// If the array is full extend the size of the array
+			this.extendArrayCheck();
 			
-			for (int i = this.size(); i > 0; i--) {
+			for (int i = this.maximumSizeOfArray; i > 0; i--) {
 				int newIndex = i + 1;
-				System.out.println(newIndex);
-				this.items[newIndex] = this.items[i];
+				if (newIndex < this.maximumSizeOfArray) {
+					this.items[newIndex] = this.items[i];
+				}
 			}
 
 			this.items[index] = object;
 
-			return new ReturnObjectImpl();
+			return new ReturnObjectImpl(null);
 		}
 	}
 
@@ -77,14 +88,39 @@ public class ArrayList implements List {
 		if (object == null) {
 			return new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT);
 		} else {
-			for (int i = 0; i < this.size; i++) {
+			// If the array is full extend the size of the array
+			this.extendArrayCheck();
+
+			for (int i = 0; i < this.maximumSizeOfArray; i++) {
 				if (this.items[i] == null) {
 					this.items[i] = object;
-					return new ReturnObjectImpl();
+					return new ReturnObjectImpl(null);
 				}
 			}
 
 			return new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS);
 		}
+	}
+
+	private void extendArrayCheck() {
+		if (this.maximumSizeOfArray == this.size()) {
+			this.extendArray();
+		}
+	}
+
+	private void extendArray() {
+		Object[] oldArray 		= this.items;
+		int newSize 			= this.sizeOfArray + this.size();
+		this.maximumSizeOfArray = newSize;
+		this.items 		  		= new Object[newSize];
+		int l 			  		= oldArray.length;
+
+		for (int i = 0; i < l; i++) {
+			this.items[i] = oldArray[i];
+		}
+	}
+
+	public void debugArray() {
+		System.out.println(Arrays.toString(this.items));
 	}
 }
